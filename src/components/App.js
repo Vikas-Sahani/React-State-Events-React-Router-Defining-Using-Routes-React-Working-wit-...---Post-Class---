a103af98b16df97a4111db3acec659c77caed862
Vikas <102462411+Vikas-Sahani@users.noreply.github.com>
2022-06-12 09:04:04 +0530

    used flag, Marco, polo var to run

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import '../styles/App.css';
 const App = () => {
+  const [flag, setFlag] = useState(false);
+  const [Marco, setMarco] = useState("Marco");
+  const [Polo, setPolo] = useState("Polo");
   
   return (
     <div id="main">
-
+      <h1 id="marco-polo">{Marco}</h1>
+      <button id="marco-polo-toggler"  onClick={function(){
+        if(flag){
+          setFlag(false);
+          setMarco("Polo");
+          setPolo("Marco");
+        }
+        else{
+          setFlag(true);
+          setMarco("Marco");
+          setPolo("Polo");
+        }
+      }}>{Polo}</button>
     </div>
   )
 }
